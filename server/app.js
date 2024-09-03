a5af396b2b670aa7d189f2c557e0d0ac42b1de5c
elvis-visi <sivisevi1@gmail.com>
2024-09-03 16:36:07 +0100

    login controller

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import usersRouter from './controllers/users.js';
 import transcriptionsRouter from './controllers/transcriptions.js';
+import loginRouter from './controllers/login.js';
 
 const app = express();
 
@@ -17,5 +20,6 @@ app.use(express.json());
 // Add routes here
 app.use('/api/users', usersRouter)
 app.use('/api/transcriptions',transcriptionsRouter)
+app.use('/api/login',loginRouter)
 
 export default app;
